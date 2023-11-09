52d6c427034bd5255ba85b359e7ce75a8e1f8a64
akorede <emiolasaheed1@gmail.com>
2023-11-09 15:28:48 +0100

    Update the EventsController and corresponding routes to allow you to create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Event was created sucessfully!"
+      redirect_to user_path(current_user)
+    else
+      flash.now[:error] = "Event was not created!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :event_date, :location)
+  end
 end
